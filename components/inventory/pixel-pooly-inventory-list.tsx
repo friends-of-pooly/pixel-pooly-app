141aa536d2bad4b5c148d2d50a33a1646535a744
Ryan McPeck <mcoso.dev@gmail.com>
2023-03-10 08:24:36 -0500

    fix: rarity tag in inventory

--- components/inventory/pixel-pooly-inventory-list.tsx
+++ components/inventory/pixel-pooly-inventory-list.tsx
@@ -36,7 +36,7 @@ export const PixelPoolyInventoryList = ({ className, tokenId, ownedTraits, chara
       {ownedTraits.map((item, index) => {
         const isEquiped = character.find((x) => x.frame == item.frame && x.layer == item.layer)
         const itemClasses = classNames(
-          'mb-2 col-span-8 lg:col-span-6 grid cursor-pointer grid-cols-12 gap-x-4 rounded-lg bg-neutral-100 hover:bg-neutral-200 p-4 dark:hover:bg-neutral-900 dark:bg-neutral-800',
+          'relative mt-2 mb-2 col-span-8 lg:col-span-6 grid cursor-pointer grid-cols-12 gap-x-4 rounded-lg bg-neutral-100 hover:bg-neutral-200 p-4 dark:hover:bg-neutral-900 dark:bg-neutral-800',
           {
             'border border-4 border-b-8 border-indigo-500/100': isEquiped,
           }
@@ -49,6 +49,9 @@ export const PixelPoolyInventoryList = ({ className, tokenId, ownedTraits, chara
             <div className="flex-center col-span-12 flex">
               <PixelPoolyTraitName className="font-raleway text-xl font-bold" layer={item.layer} frame={item.frame} />
             </div>
+            <span className="absolute -right-2 -top-4">
+              <PixelPoolyTraitTier className="mt-2 text-sm" layer={item.layer} frame={item.frame} />
+            </span>
             <div className="flex-center col-span-4 flex ">
               <span className="pt-4">
                 {trait && trait.croppedSVG ? <img src={trait.croppedSVG} className="h-20 w-20" alt="owned-trait" /> : null}
@@ -56,8 +59,7 @@ export const PixelPoolyInventoryList = ({ className, tokenId, ownedTraits, chara
             </div>
             <div className="col-span-8">
               <PixelPoolyTraitLayerName className="mt-2 font-raleway text-sm font-medium text-neutral-500" layer={item.layer} frame={item.frame} />
-              <p className="text-sm">{item.traitDescription}</p>
-              <PixelPoolyTraitTier className="mt-2 text-sm" layer={item.layer} frame={item.frame} />
+              <p className="text-left text-sm">{item.traitDescription}</p>
             </div>
             <div className="flex-center col-span-12 mt-2 flex">
               {isEquiped ? null : (
